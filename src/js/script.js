7a197d78c921281c7f67e3d1e22897537a7f40a0
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-02-04 18:40:03 +0100

    add complete initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -82,27 +82,28 @@
     }
     initAccordion() {
       const thisProduct = this;
-      console.log('This element: ', thisProduct.element);
+
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = document.querySelector(
+      const clickableTrigger = thisProduct.element.querySelector(
         select.menuProduct.clickable
       );
-      console.log('klikalny: ', clickableTrigger);
 
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
         event.preventDefault();
-
         /* find active product (product that has active class) */
-        const findProducts = document.querySelectorAll(select.all.menuProducts);
-
+        const activeProduct = document.querySelector(
+          select.all.menuProductsActive
+        );
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        for (let findProduct of findProducts) {
-          console.log('klasy', findProduct.classList);
-          findProduct.classList.toggle('active');
+        if (activeProduct !== null && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
         /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(
+          classNames.menuProduct.wrapperActive
+        );
       });
     }
   }
